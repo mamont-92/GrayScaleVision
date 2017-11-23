5ed1deaa5e3d90577ceb11962c44819f5ed7900f
fedor <fedor@admin.com>
2017-11-23 15:41:38 +0300

    migrate from mix qml + qtwidgets to pure qml

--- QML/main.qml
+++ QML/main.qml
@@ -3,7 +3,7 @@ import QtQuick.Window 2.2
 import QtQuick.Controls 2.0
 import QtQml 2.2
 
-Item {
+Window {
     id: root
     visible: true
     width: 640
--- main.cpp
+++ main.cpp
@@ -1,11 +1,10 @@
-#include "MainWindow.h"
-#include <QApplication>
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
-
-    return a.exec();
+    QGuiApplication app(argc, argv);
+    QQmlApplicationEngine engine;
+    engine.load(QUrl(QStringLiteral("qrc:/QML/main.qml")));
+    return app.exec();
 }
