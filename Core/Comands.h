3b1dfa5179e252a09ea9ab4fc73e41ff92a0b1bc
fedor <gigaslon@yandex.ru>
2017-12-27 21:39:02 +0300

    add Commands and Iterface for command's visitor/acceptor

---
+++ Core/Comands.h
@@ -0,0 +1,99 @@
+#ifndef COMANDS_H
+#define COMANDS_H
+
+#include <QString>
+#include <QVariant>
+
+namespace FilterProcessorComands {
+
+class ICommandAcceptor; //interface for "visitor pattern"
+
+class ICommand // interface for "command pattern"
+{
+protected:
+    ICommand();
+public:
+    virtual ~ICommand();
+    virtual void accept(ICommandAcceptor * _visitor) = 0;
+};
+
+class AddFilter : public ICommand
+{
+public:
+    AddFilter(const int & _id, const QString & _name);
+    void accept(ICommandAcceptor * _visitor);
+
+    int id() const;
+    QString name() const;
+private:
+    int m_id;
+    QString m_name;
+};
+
+class RemoveFilter : public ICommand
+{
+public:
+    RemoveFilter(const int & _id);
+    void accept(ICommandAcceptor * _visitor);
+
+    int id() const;
+private:
+    int m_id;
+};
+
+
+class ConnectFilters : public ICommand
+{
+public:
+    ConnectFilters(const int & _filterOut, const int & _connectorOut,
+                   const int & _filterIn, const int & _connectorIn);
+    void accept(ICommandAcceptor * _visitor);
+
+    int filterOut() const;
+    int connectorOut() const;
+    int filterIn() const;
+    int connectorIn() const;
+private:
+    int m_filterOut, m_connectorOut, m_filterIn, m_connectorIn;
+};
+
+class SetParameterForFilter : public ICommand
+{
+public:
+    SetParameterForFilter(const int & _filterNumber, const QString & _paramName, const QVariant & _value);
+    void accept(ICommandAcceptor * _visitor);
+
+    int filterNumber() const;
+    QString paramName() const;
+    QVariant value() const;
+private:
+    int m_filterNumber;
+    QString m_paramName;
+    QVariant m_value;
+};
+
+
+class SetRasterMode : public ICommand
+{
+public:
+    SetRasterMode(const QString & _mode);
+    void accept(ICommandAcceptor * _visitor);
+
+    QString mode() const;
+private:
+    QString m_mode;
+};
+
+class ICommandAcceptor
+{
+    virtual ~ICommandAcceptor();
+    virtual void accept(const AddFilter & command) = 0;
+    virtual void accept(const RemoveFilter & command) = 0;
+    virtual void accept(const ConnectFilters & command) = 0;
+    virtual void accept(const SetParameterForFilter & command) = 0;
+    virtual void accept(const SetRasterMode & command) = 0;
+};
+
+}
+
+#endif // COMANDS_H
